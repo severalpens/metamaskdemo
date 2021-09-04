93b4745eecb29e46c973dfed0d5c857e40c1104a
severalpens <severalpens@protonmail.com>
2021-09-04 18:18:28 +1000

    format doc and add headers

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ import t1_generateAccounts from './scripts/t1_generateAccounts';
 import { t2_getBalance } from './scripts/t2_getBalance';
 import { t3_transferEth } from './scripts/t3_transferEth';
 
-// t1_generateAccounts();
-// t2_getBalance();
-// t3_transferEth();
-
-let globalResults = {results: 'results'};
+let globalResults = { results: 'results' };
 
 class App extends react.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      results: 'results'
-    }
-    
+    this.state = { status: '' }
     this.t1 = this.t1.bind(this);
     this.t2 = this.t2.bind(this);
     this.t3 = this.t3.bind(this);
@@ -25,50 +18,66 @@ class App extends react.Component {
 
   async t1(e) {
     e.preventDefault();
-    this.setState({ results: 'fetching..' });
-    let resultsObject = await t1_generateAccounts();
-    globalResults = resultsObject;
-    let results = JSON.stringify(resultsObject)
-    this.setState({ results});
+    this.setState({ status: 'running t1_generateAccounts..' });
+    globalResults = await t1_generateAccounts();
+    this.setState({ status: 'completed t1_generateAccounts' });
   }
 
   async t2(e) {
     e.preventDefault();
-    this.setState({ results: 'fetching..' });
-    let results = await t2_getBalance();
-    this.setState({ results });
+    this.setState({ status: 'running t2_getBalance..' });
+    globalResults = await t2_getBalance();
+    this.setState({ status: 'completed t2_getBalance' });
   }
+
 
   async t3(e) {
     e.preventDefault();
-    this.setState({ results: 'fetching..' });
-    let results = await t3_transferEth();
-    this.setState({ results });
+    this.setState({ status: 'running t3_transferEth..' });
+    globalResults = await t3_transferEth();
+    this.setState({ status: 'completed t3_transferEth' });
   }
 
   render() {
     return (
       <div className="App">
-        <div className="section">
-          <div className="tx">
-            <div>t1_generateAccounts: </div>
-            <button id="t1" onClick={this.t1}>Generate Accounts</button>
-          </div>
-          <div className="tx">
-            <div>t2_getBalance: </div>
-            <button id="t2" onClick={this.t2}>Get Balance</button>
-          </div>
-          <div className="tx">
-            <div>t3_transferEth: </div>
-            <button id="t3" onClick={this.t3}>Transfer Eth</button>
+        <div className="header">
+          <div>
+            <div>
+              <h1>
+                Demo of <a href="https://docs.ethers.io/" target="blank"> Ethers.js </a> and <a href="https://metamask.io" target="blank"> Metamask </a>.
+              </h1>
+
+            </div>
+            <div>
+              <p>See <a href="https://youtube.com/playlist?list=PLH7-GRFbhInaXsLjTgYdfiU7k7yPj3fxT" target="blank">youtube playlist</a>for more info.</p>
+
+            </div>
           </div>
         </div>
-        <div className="section">
-          <pre>
-        {JSON.stringify(globalResults, null, 2)}       
+        <div className="container">
+          <div className="section">
+            <div className="tx">
+              <div>t1_generateAccounts: </div>
+              <button id="t1" onClick={this.t1}>Generate Accounts</button>
+            </div>
+            <div className="tx">
+              <div>t2_getBalance: </div>
+              <button id="t2" onClick={this.t2}>Get Balance</button>
+            </div>
+            <div className="tx">
+              <div>t3_transferEth: </div>
+              <button id="t3" onClick={this.t3}>Transfer Eth</button>
+            </div>
+          </div>
+          <div className="section">
+            <div>status: {this.state.status}</div>
+            <pre>
+              {JSON.stringify(globalResults, null, 2)}
 
-          </pre>
-         </div>
+            </pre>
+          </div>
+        </div>
       </div>
     );
 
--- src/scripts/t3_transferEth.js
+++ src/scripts/t3_transferEth.js
@@ -3,13 +3,16 @@ import {chalk} from "chalk";
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
-const account2 = "0xA0e2142EA0b15476bD059071625675B06aDA172c";
+const account2 =   {
+  address: "0x1fd7a4B0f572e8D3483d5D42437FF541C0CD51dD",
+  privateKey: "0xf9c5b2d93f1a71ef0ea796623a41a26519dd40525b612820b6c22c09e03920c8"
+}
 
 export const t3_transferEth = async function () {
 
   let tx = await signer.sendTransaction({
-    to: account2,
-    value: ethers.utils.parseEther("0.01")
+    to: account2.address,
+    value: ethers.utils.parseEther("0.001")
   });
 
   console.log("Transaction request successfully sent! See Etherscan for details:");
@@ -20,5 +23,6 @@ export const t3_transferEth = async function () {
   delete(completedTransaction.logsBloom);
   console.log("Transaction request successfully sent! Details:");
  console.log(completedTransaction);
+ return completedTransaction;
 
 }
